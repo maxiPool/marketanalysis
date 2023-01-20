745c366875f3eb26a271276e78fc5287b7164aa8
maxiPool <maxime.berard@gmail.com>
2023-01-19 22:19:46 -0500

    initial commit

---
+++ src/main/kotlin/max/demo/marketanalysis/MarketAnalysisApplication.kt
@@ -0,0 +1,11 @@
+package max.demo.marketanalysis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketAnalysisApplication
+
+fun main(args: Array<String>) {
+    runApplication<MarketAnalysisApplication>(*args)
+}
