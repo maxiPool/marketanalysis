ea9371112bb90685723e56b4b1557c2255c852bb
maxiPool <maxime.berard@gmail.com>
2023-01-19 22:28:45 -0500

    chore: enforce formatting

--- src/main/kotlin/max/demo/marketanalysis/MarketAnalysisApplication.kt
+++ src/main/kotlin/max/demo/marketanalysis/MarketAnalysisApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class MarketAnalysisApplication
 
 fun main(args: Array<String>) {
-    runApplication<MarketAnalysisApplication>(*args)
+  runApplication<MarketAnalysisApplication>(*args)
 }
