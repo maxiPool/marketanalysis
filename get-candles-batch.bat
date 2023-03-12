e299eeed3b13e9688cb16ded94e476d64072ecc8
maxiPool <maxime.berard@gmail.com>
2023-03-12 12:44:50 -0400

    Update get-candles-batch.bat

--- get-candles-batch.bat
+++ get-candles-batch.bat
@@ -1,1 +1,4 @@
-.\mvnw package -DskipTests=true && java -jar .\target\market-analysis-0.0.1-SNAPSHOT.jar --spring.profiles.active=local
+echo "Running get candles batch job" > batch-job-output.txt && ^
+.\mvnw package -DskipTests=true >> batch-job-output.txt && ^
+java -jar .\target\market-analysis-0.0.1-SNAPSHOT.jar --spring.profiles.active=local >> batch-job-output.txt && ^
+echo "DONE!" >> batch-job-output.txt
